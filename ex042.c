18cab3bbe9ef6ffbd4e6edb920ace04d377c18a9
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-09-09 22:08:01 -0300

    Create ex042.c

---
+++ ex042.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    char nome[25], sexo;
+    int numero_pessoa, total_homens = 0, total_mulheres = 0;
+
+    for (numero_pessoa = 1; numero_pessoa <= 5; numero_pessoa++)
+    {
+        printf("Entre com seu nome:");
+        scanf("%s", nome);
+        printf("Entre com o sexo:");
+        scanf(" %c", &sexo);
+        if (sexo == 'm')
+        {
+            total_homens++;
+        }
+        else
+        {
+            total_mulheres++;
+        }
+    }
+    printf("Total de homens: %d\n", total_homens);
+    printf("Total de mulheres: %d", total_mulheres);
+    return 0;
+}
