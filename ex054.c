691008d807b9b046bb06cc0e3cb44b9810aed53e
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-09-09 22:08:10 -0300

    Create ex054.c

---
+++ ex054.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char palavra1[20], palavra2[20];
+    int tamanho_palavra1, tamanho_palavra2;
+
+    printf("Entre com uma palavra:");
+    scanf("%s", palavra1);
+    printf("Entre com outra palavra:");
+    scanf("%s", palavra2);
+    tamanho_palavra1 = strlen(palavra1);
+    tamanho_palavra2 = strlen(palavra2);
+    printf("A primeira palavra possui %d caracteres.\n", tamanho_palavra1);
+    printf("A segunda palavra possui %d caracteres.\n", tamanho_palavra2);
+    if (tamanho_palavra1 < tamanho_palavra2)
+    {
+        printf("Palavra menor: %s\n", palavra1);
+    }
+    else
+    {
+        if (tamanho_palavra2 < tamanho_palavra1)
+        {
+            printf("Palavra menor: %s\n", palavra2);
+        }
+        else
+        {
+            printf("As palavras possuem tamanhos iguais.\n");
+        }
+    }
+    return 0;
+}
