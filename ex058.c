bf61ba8b71da37b88d14bd7c4009921b5023da05
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-09-10 23:46:33 -0300

    Create ex058.c

---
+++ ex058.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+int main()
+{
+	char palavra[20];
+    int posicao,contador;
+
+    printf("digite uma palavra:");
+    scanf("%s", palavra);
+    posicao = strlen(palavra)-1;
+    while(posicao>=0)
+    {
+    printf("%c",toupper(palavra[posicao]));
+    posicao --;
+    }
+	printf("\n");
+	
+    system("pause");
+}
