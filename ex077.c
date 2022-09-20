c8accf8308335e16aca0b09ac3fed94394f7be54
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-09-19 23:59:07 -0300

    Create ex077.c

---
+++ ex077.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+#define TAM_MAX 5
+
+int main()
+{
+    int vetor_a[TAM_MAX], vetor_b[TAM_MAX], vetor_c[TAM_MAX * 2], indice1, indice2;
+
+    for (indice1 = 0; indice1 < TAM_MAX; indice1++)
+    {
+        printf("Entre com um numero para a posição %d do vetor_a:", indice1);
+        scanf("%d", &vetor_a[indice1]);
+    }
+    for (indice1 = 0; indice1 < TAM_MAX; indice1++)
+    {
+        printf("Entre com um numero para a posição %d do vetor_b:", indice1);
+        scanf("%d", &vetor_b[indice1]);
+    }
+    indice2 = 0;
+    for (indice1 = 0; indice1 < TAM_MAX; indice1++)
+    {
+        vetor_c[indice2] = vetor_a[indice1];
+        indice2++;
+        vetor_c[indice2] = vetor_b[indice1];
+        indice2++;
+    }
+    printf("\n\n");
+    for (indice2 = 0; indice2 < TAM_MAX * 2; indice2++)
+    {
+        printf("%d\n", vetor_c[indice2]);
+    }
+    return 0;
+}
