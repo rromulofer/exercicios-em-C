3cee4f5b08156ccdf9fdbdc4f93393b360baf0ce
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-09-28 21:22:59 -0300

    update

---
+++ MatrizPLP.cpp
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int main(){
+
+  int A[4][4], B[4][4] ,C[4][4];
+
+  // Obter martriz A
+  for(int i=0;i<4;++i){
+   for(int j=0;j<4;++j){
+     printf("A[%d][%d]: ",i,j);
+     scanf("%d", &A[i][j]);
+   }
+  }
+
+  printf("\n");
+
+  // Obter martriz B
+  for(int i=0;i<4;++i){
+   for(int j=0;j<4;++j){
+     printf("B[%d][%d]: ",i,j);
+     scanf("%d", &B[i][j]);
+   }
+  }
+ 
+  printf("\n");
+
+  // Calcular e mostrar a soma
+  for(int i=0;i<4;++i){
+   for(int j=0;j<4;++j){
+     C[i][j] = A[i][j] + B[i][j];
+     printf("%d\t", C[i][j]);
+      if (j==3) printf("\n");
+   }
+  }
+ 
+ printf("\n");
+ printf("\n");
+ 
+ // Calcular e mostrar o produto
+  for(int i=0;i<4;++i){
+   for(int j=0;j<4;++j){
+     C[i][j] = A[i][j] * B[i][j];
+     printf("%d\t", C[i][j]);
+      if (j==3) printf("\n");
+   }
+  }
+  printf("\n");
+
+ }
