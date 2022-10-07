27c6b296ad185490d2b3563a6b07225338116765
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-10-06 23:30:57 -0300

    update

---
+++ ex95b.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int funcaosoma(int numero1, int numero2, int numero3)
+{
+	int resultado;
+	
+	resultado=numero1+numero2+numero3;
+	
+	
+	return resultado;
+}
+
+
+int main()
+{
+	int n1, n2, n3, soma;
+	
+	printf("digite um valor: ");
+	scanf("%d", &n1);
+	
+	printf("digite outro valor: ");
+	scanf("%d", &n2);
+	
+	printf("digite outro valor: ");
+	scanf("%d", &n3);
+	
+	soma= funcaosoma(n1, n2, n3);
+	
+	printf("resultado da soma: %d ", soma);
+	
+	return 0;
+}
