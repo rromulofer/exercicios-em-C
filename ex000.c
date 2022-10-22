153b87fa49c020b54fe444fbda72120f7d8c708c
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-10-21 22:55:11 -0300

    update

--- ex000.c
+++ ex000.c
@@ -1,3 +1,10 @@
+/*
+UENF - CCT - CC
+Paradigmas de Linguagens de Programação
+Aluno: Rômulo Souza Fernandes
+E-mail: 00119110559@pq.uenf.br
+Data de criação: 20/10/22 
+*/
 #include<stdlib.h>
 
 struct menor
